0ac6c7d05236f023ae977aca1efeb1d16aaf0412
Carl Saldanha <cjds@live.com>
2021-03-26 00:09:31 +0000

    share link basics

--- packages/webviz-core/src/components/LayoutMenu.js
+++ packages/webviz-core/src/components/LayoutMenu.js
@@ -6,13 +6,15 @@
 //  found in the LICENSE file in the root directory of this source tree.
 //  You may not use this file except in compliance with the License.
 import CodeJsonIcon from "@mdi/svg/svg/code-json.svg";
+import ShareIcon from "@mdi/svg/svg/share.svg";
+
 import React, { useState } from "react";
 
 import LayoutIcon from "webviz-core/src/assets/layout.svg";
 import ChildToggle from "webviz-core/src/components/ChildToggle";
 import Flex from "webviz-core/src/components/Flex";
 import { WrappedIcon } from "webviz-core/src/components/Icon";
-import { openLayoutModal } from "webviz-core/src/components/LayoutModal";
+import { openLayoutModal, openShareModal } from "webviz-core/src/components/LayoutModal";
 import Menu, { Item } from "webviz-core/src/components/Menu";
 import { ClearBagCacheMenuItem } from "webviz-core/src/util/indexeddb/clearIndexedDb";
 
@@ -33,7 +35,15 @@ export default function LayoutMenu() {
             setIsOpen(false);
             openLayoutModal();
           }}>
-          Import/export layout
+          Import/Export Layout
+        </Item>
+        <Item
+          icon={<ShareIcon />}
+          onClick={() => {
+            setIsOpen(false);
+            openShareModal();
+          }}>
+          Generate URL of Scene
         </Item>
         <ClearBagCacheMenuItem />
       </Menu>
